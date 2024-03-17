bb2f8914e37090dd2f1c9eb032c07171e305993c
molalenim <molaleni.mabaso@gmail.com>
2024-03-17 19:29:43 -0400

    Graded Assignment #6: Orbit Report - task 4 & 5

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,17 @@
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {/* First button */}
+      {displaySats.map((sat, id) => (
+        <button key={id} onClick={() => filterByType(sat)}>
+          {sat} Orbit
+        </button>
+      ))}
+
+      {/* Second button */}
+      <button onClick={() => setSat(satData)}>All Satellites</button>
     </div>
   );
 };
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,18 +1,30 @@
 const Table = ({ sat }) => {
   return (
     <table>
-    <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
-     </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* Iterate through the sat prop using map */}
+        {sat.map((data, id) => (
+          <tr key={id}>
+            {/* Render data for each row */}
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            {/* Conditional rendering based on status */}
+            <td>{data.status ? "Active" : "Inactive"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
 export default Table;
+

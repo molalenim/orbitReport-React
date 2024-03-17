9aad956c55495530eea79ea551466ba75c69837c
molalenim <molaleni.mabaso@gmail.com>
2024-03-17 12:02:03 -0400

    Graded Assignment #6: Orbit Report - Task 2

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+const Banner = () => {
+    return (
+        <div>
+            <header> 
+                "Orbit Report"
+            </header>
+            <p>
+                Click on the buttons to see the satellites in that orbit type
+
+            </p>
+        </div>
+    );
+};
+
+export default Banner;
